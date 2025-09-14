e7099b1e446a6d74612e459264a842b734b11af1
falak964 <mehtafalak14@gmail.com>
2025-09-14 15:21:22 +0530

    init_db.py

--- hospital_management/init_db.py
+++ hospital_management/init_db.py
@@ -1,22 +1,17 @@
 import sqlite3
 
-# Connect to your database (or create if not exists)
+# Connect database
 conn = sqlite3.connect("hospital.db")
 c = conn.cursor()
 
-# Create the user table
-c.execute('''
-CREATE TABLE IF NOT EXISTS user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-)
-''')
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)''')
 
-# Add a sample user (optional)
+#sample user
 c.execute("INSERT OR IGNORE INTO user (username, password) VALUES (?, ?)", ("admin", "admin123"))
 
 conn.commit()
 conn.close()
 
 print("Database and 'user' table created successfully.")
+
